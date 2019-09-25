168b7cefb59c247985a46e706cf5bd81bebd674e
ccc870206 <ccc870206@gmail.com>
2019-09-25 17:35:07 +0800

    make code more clear to see

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -9,8 +9,7 @@ def load_data():
         for line in f.readlines():
             input_file.append(line.strip().split(','))
     input_file = np.asarray(input_file, dtype=float)
-    # return input_file, int(input('input n:')), float(input('input lambda:'))
-    return input_file, 2, 0
+    return input_file, int(input('input n:')), float(input('input lambda:'))
 
 
 def build_design_matrix(n, x):
@@ -21,18 +20,26 @@ def build_design_matrix(n, x):
     return design_matrix
 
 
-def build_identity_matrix(n,lda):
-    identity_matrix = np.zeros((n,n))
+def build_identity_matrix(n, lda):
+    identity_matrix = np.zeros((n, n))
     for i in range(n):
         identity_matrix[i][i] = lda
     return identity_matrix
+
+
+def build_random_matrix(n):
+    init_x = np.zeros((n, 1))
+    for i in range(n):
+        init_x[i][0] = random.uniform(0, 5)
+
+    return init_x
 
 
 def LU_factorization(matrix):
     rank = matrix.shape[0]
     lower = build_identity_matrix(rank, 1)
 
-    for row in range(1,rank):
+    for row in range(1, rank):
         for col in range(row):
             e = matrix[row][col] / matrix[col][col]
             lower[row][col] = e
@@ -57,33 +64,24 @@ def backward_substitution(matrix, target):
     rank = matrix.shape[0]
     solution = np.empty((rank, 1))
 
-    for row in range(rank-1, -1, -1):
+    for row in range(rank - 1, -1, -1):
         sum_var = 0
-        for col in range(row+1, rank):
+        for col in range(row + 1, rank):
             sum_var += solution[col] * matrix[row][col]
         solution[row] = (target[row] - sum_var) / matrix[row][row]
 
     return solution
 
 
-def output(para, total_error):
-    equation = ''
-    for i in range(para.shape[0] - 1,-1,-1):
-        if i == 0:
-            equation += (str(para[i][0]))
-        else:
-            equation += (str(para[i][0]) + ' x^' + str(i) + ' + ')
+def compute_LSE(A, b):
+    At = np.transpose(A)
+    AtxA = np.matmul(At, A) + build_identity_matrix(n, lda)
+    Atxb = np.matmul(At, b)
 
-    print('Fitting line:', equation)
-    print('Total error:', total_error)
-
-
-def initial_matrix(n):
-    init_x = np.zeros((n,1))
-    for i in range(n):
-        init_x[i][0] = random.uniform(0,5)
-
-    return init_x
+    L, U = LU_factorization(AtxA)
+    y = forward_substitution(L, Atxb)
+    x = backward_substitution(U, y)
+    return x
 
 
 def compute_f(A, At, x, xt, b, bt):
@@ -111,7 +109,7 @@ def compute_H(A, At):
     return 2 * np.matmul(At, A)
 
 
-def compute_step(A, b, x):
+def compute_newton(A, b, x):
     xt = np.transpose(x)
     At = np.transpose(A)
     bt = np.transpose(b)
@@ -123,76 +121,72 @@ def compute_step(A, b, x):
 
     y = forward_substitution(L, gradient)
     inverse = backward_substitution(U, y)
-    # inverse = np.empty((n, n))
-    # for i in range(f.shape[1]):
-    #     y = forward_substitution(L, gradient[:,i])
-    #     x = backward_substitution(U, y)
-    #     inverse[:, i] = np.transpose(x)
-
-    # print(inverse)
 
     return x - inverse
-    # print(H)
-    # print(gradient)
+
 
 def draw_equation(para):
     x = np.linspace(-5, 5, 100)
     y = np.zeros(100)
-    print('x', x.shape)
-    #
-    # print((para[1][0]*np.power(x,1) + y).shape)
-    # print((para[1][0]*np.power(x,1) + y).shape)
+
     for i in range(para.shape[0]):
         y += (para[i][0] * np.power(x, i))
 
     plt.plot(x, y, color="black")
-    # plt.show()
 
 
-def LSE():
+def draw_groundtruth(x, y):
+    plt.scatter(x, y)
+
+
+def print_result(para, total_error):
+    equation = ''
+    for i in range(para.shape[0] - 1, -1, -1):
+        if i == 0:
+            equation += (str(para[i][0]))
+        else:
+            equation += (str(para[i][0]) + ' x^' + str(i) + ' + ')
+
+    print('Fitting line:', equation)
+    print('Total error:', total_error)
+
+
+def output(A, b, data_x, para):
+    predict_y = np.matmul(A, para)
+    error = np.sum(np.power(predict_y[:, 0] - b, 2))
+
+    draw_groundtruth(data_x, b)
+    draw_equation(para)
+    print_result(para, error)
+
+
+def LSE(A, b):
+    result = compute_LSE(A, b)
+
+    print('LSE:')
+    output(A, b, data[:, 0], result)
+
+
+def Newton_method(A, b):
+    x = build_random_matrix(n)
+
+    result = compute_newton(A, b, x)
+
+    print('Newton\'s Method:')
+    output(A, b, data[:, 0], result)
+
+
+if __name__ == "__main__":
     data, n, lda = load_data()
     A = build_design_matrix(n, data[:, 0])
     b = data[:, 1]
-    # print(data)
-    At = np.transpose(A)
-    AtxA = np.matmul(At, A) + build_identity_matrix(n, lda)
-    Atxb = np.matmul(At, b)
 
-    L, U = LU_factorization(AtxA)
-    y = forward_substitution(L, Atxb)
-    x = backward_substitution(U, y)
-    # print(x)
+    fig = plt.subplots(nrows=2, ncols=1)
+    plt.subplot(211)
+    plt.title('LSE')
+    LSE(A, b)
 
-    F = np.matmul(A, x)
-    plt.scatter(data[:,0],F)
-
-    error = np.sum(np.power(F[:, 0] - b, 2))
-    # print('error:',error)
-    print('LSE:')
-    output(x, error)
-
-    plt.scatter(data[:, 0],b)
-
+    plt.subplot(212)
+    plt.title('Newton')
+    Newton_method(A, b)
     plt.show()
-
-
-data, n, lda = load_data()
-x = initial_matrix(n)
-# print(x)
-A = build_design_matrix(n, data[:, 0])
-b = data[:, 1]
-result = compute_step(A, b, x)
-
-print(result)
-draw_equation(result)
-
-F = np.matmul(A, result)
-#
-#
-error = np.sum(np.power(F[:, 0] - b, 2))
-print('Newton\'s Method::')
-output(result, error)
-
-plt.scatter(data[:, 0], b)
-
-plt.show()
