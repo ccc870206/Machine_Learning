40382e6e17f303a5526ed511818998f2c82cd245
ccc870206 <ccc870206@gmail.com>
2019-09-25 16:30:14 +0800

    newton's method complete

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 def load_data():
@@ -8,8 +9,8 @@ def load_data():
         for line in f.readlines():
             input_file.append(line.strip().split(','))
     input_file = np.asarray(input_file, dtype=float)
-    return input_file, int(input('input n:')), float(input('input lambda:'))
-    # return input_file, 3, 10000
+    # return input_file, int(input('input n:')), float(input('input lambda:'))
+    return input_file, 2, 0
 
 
 def build_design_matrix(n, x):
@@ -25,6 +26,7 @@ def build_identity_matrix(n,lda):
     for i in range(n):
         identity_matrix[i][i] = lda
     return identity_matrix
+
 
 def LU_factorization(matrix):
     rank = matrix.shape[0]
@@ -75,27 +77,122 @@ def output(para, total_error):
     print('Fitting line:', equation)
     print('Total error:', total_error)
 
+
+def initial_matrix(n):
+    init_x = np.zeros((n,1))
+    for i in range(n):
+        init_x[i][0] = random.uniform(0,5)
+
+    return init_x
+
+
+def compute_f(A, At, x, xt, b, bt):
+    # xt * At * A * x
+    first = np.matmul(np.matmul(np.matmul(xt, At), A), x)
+    # 2 * xt * At * b
+    second = 2 * np.matmul(np.matmul(xt, At), b)
+    # bt * b
+    third = np.matmul(bt, b)
+
+    return first - second + third
+
+
+def compute_gradient(A, At, b, x):
+    # 2 * At * A * x
+    first = 2 * np.matmul(np.matmul(At, A), x)
+
+    # 2 * At * b
+    second = 2 * np.matmul(At, b)
+
+    return first[:, 0] - second
+
+
+def compute_H(A, At):
+    return 2 * np.matmul(At, A)
+
+
+def compute_step(A, b, x):
+    xt = np.transpose(x)
+    At = np.transpose(A)
+    bt = np.transpose(b)
+    f = compute_f(A, At, x, xt, b, bt)
+    gradient = compute_gradient(A, At, b, x)
+    # print('gradient', gradient)
+    H = compute_H(A, At)
+    L, U = LU_factorization(H)
+
+    y = forward_substitution(L, gradient)
+    inverse = backward_substitution(U, y)
+    # inverse = np.empty((n, n))
+    # for i in range(f.shape[1]):
+    #     y = forward_substitution(L, gradient[:,i])
+    #     x = backward_substitution(U, y)
+    #     inverse[:, i] = np.transpose(x)
+
+    # print(inverse)
+
+    return x - inverse
+    # print(H)
+    # print(gradient)
+
+def draw_equation(para):
+    x = np.linspace(-5, 5, 100)
+    y = np.zeros(100)
+    print('x', x.shape)
+    #
+    # print((para[1][0]*np.power(x,1) + y).shape)
+    # print((para[1][0]*np.power(x,1) + y).shape)
+    for i in range(para.shape[0]):
+        y += (para[i][0] * np.power(x, i))
+
+    plt.plot(x, y, color="black")
+    # plt.show()
+
+
+def LSE():
+    data, n, lda = load_data()
+    A = build_design_matrix(n, data[:, 0])
+    b = data[:, 1]
+    # print(data)
+    At = np.transpose(A)
+    AtxA = np.matmul(At, A) + build_identity_matrix(n, lda)
+    Atxb = np.matmul(At, b)
+
+    L, U = LU_factorization(AtxA)
+    y = forward_substitution(L, Atxb)
+    x = backward_substitution(U, y)
+    # print(x)
+
+    F = np.matmul(A, x)
+    plt.scatter(data[:,0],F)
+
+    error = np.sum(np.power(F[:, 0] - b, 2))
+    # print('error:',error)
+    print('LSE:')
+    output(x, error)
+
+    plt.scatter(data[:, 0],b)
+
+    plt.show()
+
+
 data, n, lda = load_data()
+x = initial_matrix(n)
+# print(x)
 A = build_design_matrix(n, data[:, 0])
 b = data[:, 1]
-# print(data)
-At = np.transpose(A)
-AtxA = np.matmul(At, A) + build_identity_matrix(n, lda)
-Atxb = np.matmul(At, b)
+result = compute_step(A, b, x)
 
-L, U = LU_factorization(AtxA)
-y = forward_substitution(L, Atxb)
-x = backward_substitution(U, y)
-# print(x)
+print(result)
+draw_equation(result)
 
-F = np.matmul(A, x)
-plt.scatter(data[:,0],F)
-
+F = np.matmul(A, result)
+#
+#
 error = np.sum(np.power(F[:, 0] - b, 2))
-# print('error:',error)
-print('LSE:')
-output(x, error)
+print('Newton\'s Method::')
+output(result, error)
 
-plt.scatter(data[:, 0],b)
+plt.scatter(data[:, 0], b)
 
 plt.show()
