43d4d590aa264917a54dca2d24e4d78f1d2410be
ccc870206 <ccc870206@gmail.com>
2019-10-14 20:37:10 +0800

    homework version

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -185,8 +185,7 @@ def compute_continuous_probability(train_image, train_label):
 
     # construct dict_label
     for i in range(10):
-        dict_label[i] = (label_sum[i] / 28 / 28 / label_num[i],
-                         pow(label_square[i] / 28 / 28 / label_num[i] - pow(label_sum[i] / 28 / 28 / label_num[i], 2), 1/2))
+        dict_label[i] = label_num[i] / total
         dict_pixel_cond[i] = {}
     # construct dict_pixel_cond
     for i in range(10):
@@ -279,63 +278,29 @@ def label_prediction_continuous(img_array, labels, dict_pixel, dict_label, dict_
     total = 60000
 
     wrong = 0
-    for i in range(10):
+    for i in range(10000):
 
         img = img_array[i]
         ground_truth = labels[i][0]
         posterior = np.empty(10)
         for label in range(10):
-            p_pixel = 0
             p_cond = 0
-            # p_label = dict_label[label] / total
-            # print(dict_label[1])
+
             p_label = dict_label[int(label)] / total
-            # p_label = gaussian_distribution(all[0], pow(all[1], 1/2), dict_label[label][0])
-            # print(pow(all[1], 1/2))
-            # print(dict_label[label][1])
-            # print("p_label:", p_label)
-            # print("p_label:", p_label)
+
             for row in range(img.shape[0]):
                 for col in range(img.shape[1]):
-                    """
-                    # print(row, col)
-                    # print('dict pixel cond:', dict_pixel_cond[int(label)][row*28+col][img[row][col]])
-                    # print('label:', dict_label[int(label)])
-                    p_cond[row][col] = dict_pixel_cond[int(label)][row*28+col][img[row][col]] / dict_label[int(label)]
-                    p_pixel[row][col] = dict_pixel[row*28+col][img[row][col]] / total
-                    # print(p_pixel[row][col])
-                    p_posterior[row][col] = p_cond[row][col]*p_label / p_pixel[row][col]
-                    """
-                    value = img[row][col]
-                    # p_pixel = p_pixel * dict_pixel[row*28+col][img[row][col]] / total
-                    # p_cond = p_cond * dict_pixel_cond[int(label)][row*28+col][img[row][col]] / dict_label[int(label)]
-                    # print("pixel", math.log(dict_pixel[row*28+col][value] / total))
-                    # print("cond", math.log(dict_pixel_cond[int(label)][row*28+col][value] / dict_label[int(label)]))
-
-                    # p_pixel += (math.log((dict_pixel[row * 28 + col][img[row][col]] + 10e-7) / total))
-
-                    # print(dict_pixel_cond[int(label)][row*28+col][img[row][col]] / dict_label[int(label)])
-                    # print(gaussian_distribution(dict_pixel_cond[int(label)][row * 28 + col][0],
-                    #                           dict_pixel_cond[int(label)][row * 28 + col][1],
-                    #                           img[row][col])+10e-7)
                     p_cond += (math.log(
                         gaussian_distribution(dict_pixel_cond[int(label)][row * 28 + col][0],
                                               dict_pixel_cond[int(label)][row * 28 + col][1],
                                               img[row][col]) + 10e-7))
 
             p_cond += math.log(p_label)
-            # p_cond += math.log(1/10)
-            # p_cond = p_cond * p_label
 
             posterior[label] = p_cond
-        # print(posterior)
-        # print("p_pixel:", p_pixel)
-        # print(label, p_cond)
-        # print(posterior)
+
         posterior /= (np.sum(posterior))
         output_posterior(posterior)
-        # print(posterior)
-        # print(posterior)
         prediction = np.argmin(posterior)
         if prediction != ground_truth:
             wrong += 1
@@ -343,14 +308,7 @@ def label_prediction_continuous(img_array, labels, dict_pixel, dict_label, dict_
         print("Prediction:", prediction, ", Ans:", int(ground_truth))
         print()
     return wrong / 10000
-    # print(p_cond, math.log(p_cond))
-    # print(p_pixel, math.log(p_pixel))
-    # print(p_cond / p_pixel, math.log(p_cond) / math.log(p_pixel), math.log(p_cond / p_pixel))
-    # print(pow(math.e, (p_cond / p_pixel)))
 
-    # print(np.sum(p_posterior))
-    # print("p_cond:", p_cond)
-    # print("p_pixel:", p_pixel)
 
 
 def pixel_prediction_continuous(dict_pixel, dict_label, dict_pixel_cond, all):
@@ -376,31 +334,29 @@ def discrete_mode():
 
     error_rate = label_prediction_discrete(test_image_discrete, test_label, dict_pixel, dict_label, dict_pixel_cond)
     print("Imagination of numbers in Bayesian classifier:")
+    print()
     pixel_prediction_discrete(dict_pixel, dict_label, dict_pixel_cond)
     print("Error rate:", error_rate)
 
 
 def continuous_mode():
     # Load preprocessing data
-    train_image_conti, _, test_image_conti, _, train_label, test_label = load_pkl_data()
-    dict_pixel, dict_label, dict_pixel_cond, all = load_continuous_dict()
+    # train_image_conti, _, test_image_conti, _, train_label, test_label = load_pkl_data()
+    # dict_pixel, dict_label, dict_pixel_cond, all = load_continuous_dict()
     
     # Load raw data and process
-    # train_image_conti, _, test_image_conti, _, train_label, test_label = load_data()
-    # dict_pixel, dict_label, dict_pixel_cond, all = compute_continuous_probability(train_image_conti, train_label)
+    train_image_conti, _, test_image_conti, _, train_label, test_label = load_data()
+    dict_pixel, dict_label, dict_pixel_cond, all = compute_continuous_probability(train_image_conti, train_label)
     error_rate = label_prediction_continuous(test_image_conti, test_label, dict_pixel, dict_label, dict_pixel_cond, all)
     print("Imagination of numbers in Bayesian classifier:")
+    print()
     pixel_prediction_continuous(dict_pixel, dict_label, dict_pixel_cond, all)
     print("Error rate:", error_rate)
 
 
 if __name__ == "__main__":
     option = int(input("option:"))
-    start = timeit.default_timer()
     if option == 0:
         discrete_mode()
     else:
         continuous_mode()
-    stop = timeit.default_timer()
-
-    print('Time: ', stop - start)
