ecebf7addf68446a2d94ab92fdf1106c6f92703b
ccc870206 <ccc870206@gmail.com>
2019-11-02 21:11:30 +0800

    logistic regression complete

--- hw4/logistic_regression.py
+++ hw4/logistic_regression.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import warnings
 import sys
 
-# if not sys.warnoptions:
-#     warnings.simplefilter("ignore")
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
 
 
 def generator_gaussian(mean, var):
@@ -20,16 +20,46 @@ def logistic_function(matrix):
     return matrix
 
 
+def input_data():
+    n = int(input("n:"))
+    mx1 = float(input("mx1:"))
+    my1 = float(input("my1:"))
+    mx2 = float(input("mx2:"))
+    my2 = float(input("my2:"))
+
+    vx1 = float(input("vx1:"))
+    vy1 = float(input("vy1:"))
+    vx2 = float(input("vx2:"))
+    vy2 = float(input("vy2:"))
+
+    return n, mx1, my1, mx2, my2, vx1, vy1, vx2, vy2
+
+
+def initial_parameter():
+    n, mx1, my1, mx2, my2, vx1, vy1, vx2, vy2 = input_data()
+
+    D1 = np.empty((n, 2))
+    D2 = np.empty((n, 2))
+
+    for i in range(n):
+        D1[i][0] = generator_gaussian(mx1, vx1)
+        D1[i][1] = generator_gaussian(my1, vy1)
+        D2[i][0] = generator_gaussian(mx2, vx2)
+        D2[i][1] = generator_gaussian(my2, vy2)
+
+    Dx = np.append(D1[:, 0], D2[:, 0], axis=0)
+    Dy = np.append(D1[:, 1], D2[:, 1], axis=0)
+    Dclass = np.append(np.zeros(n), np.ones(n), axis=0)
+    phi = build_design_matrix(n, Dx, Dy)
+
+    return n, Dx, Dy, Dclass, phi
+
+
 def compute_D(matrix):
-    print("m")
-    print(matrix)
     D = np.zeros((matrix.shape[1], matrix.shape[1]),dtype="float64")
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
-            # log_value = -matrix[i][j]-2*math.log()
             D[j][j] = math.e**(-matrix[i][j])/(1 + math.e**(-matrix[i][j]))/(1 + math.e ** (-matrix[i][j]))
-            # print("son", matrix[i][j], math.e**(-matrix[i][j]))
-            # print("mom", matrix[i][j], (1 + math.e**(-matrix[i][j])))
 
     return D
 
@@ -59,11 +89,11 @@ def convert_color(vector):
     color = []
     for i in vector:
         color.append('red')if i == 0 else color.append('blue')
+
     return color
 
 
 def build_confusion_matrix(gt, pred):
-    # gt pred
     c11 = 0
     c12 = 0
     c21 = 0
@@ -79,8 +109,8 @@ def build_confusion_matrix(gt, pred):
                 c21 += 1
             else:
                 c22 += 1
-    return c11, c12, c21, c22
 
+    return c11, c12, c21, c22
 
 
 def output_confusion_matrix(gt, pred):
@@ -97,122 +127,93 @@ def output_confusion_matrix(gt, pred):
     print("Specificity (Successfully predict cluster 2):", specificity)
 
 
+def predict(phi, w):
+    result = np.matmul(phi.T, w)
+
+    prediction = []
+    for i in result:
+        prediction.append(1) if i > 1 / 2 else prediction.append(0)
+
+    return prediction
 
 
-n = 50
-# mx1 = 1
-# my1 = 1
-# mx2 = 10
-# my2 = 10
-#
-# vx1 = 2
-# vy1 = 2
-# vx2 = 2
-# vy2 = 2
+def gradient_descent(phi, Dclass):
+    w = build_random_matrix(3)
 
-mx1 = 1
-my1 = 1
-mx2 = 3
-my2 = 3
-
-vx1 = 2
-vy1 = 2
-vx2 = 4
-vy2 = 4
-
-
-D1 = np.empty((n, 2))
-D2 = np.empty((n, 2))
-
-for i in range(n):
-    D1[i][0] = generator_gaussian(mx1, vx1)
-    D1[i][1] = generator_gaussian(my1, vy1)
-    D2[i][0] = generator_gaussian(mx2, vx2)
-    D2[i][1] = generator_gaussian(my2, vy2)
-
-w = build_random_matrix(3)
-Dx = np.append(D1[:, 0], D2[:, 0], axis=0)
-Dy = np.append(D1[:, 1], D2[:, 1], axis=0)
-Dclass = np.append(np.zeros(n), np.ones(n), axis=0)
-# print(D1[:, 0])
-# print(D2[:, 0])
-phi = build_design_matrix(n, Dx, Dy)
-
-
-
-
-
-
-# print(A)
-# A1 = build_design_matrix(3, D1[:, 0])
-# A2 = build_design_matrix(3, D2[:, 0])
-#
-# print(A1)
-#
-# print("Dy", Dy.shape)
-# print("Dclass:", Dclass.shape, Dclass)
-
-
-i = 0
-for i in range(50):
-    D = compute_D(np.matmul(w.T, phi))
-    print(D)
-    H = np.matmul(np.matmul(phi, D), phi.T)
-    gradient = compute_gradient(phi, Dclass, w)
-    # print(H)
-    if np.linalg.det(H) == 0:
+    while True:
+        gradient = compute_gradient(phi, Dclass, w)
         w -= gradient
-    else:
-        w -= np.matmul(np.linalg.inv(H), gradient)
-    print(w)
 
-result = np.matmul(phi.T, w)
+        if abs(np.sum(gradient)) < 3 * 10e-3:
+            break
 
-prediction = []
-for i in result:
-    prediction.append(1) if i > 1/2 else prediction.append(0)
-print(prediction)
+    prediction = predict(phi, w)
 
+    print("Gradient descent:")
+    print()
+    print("w:")
+    [print(i[0]) for i in w]
+    print()
+    output_confusion_matrix(Dclass, prediction)
 
-# ## gradient descent
-# while(True):
-# # for i in range(1000):
-#     gradient = compute_gradient(phi, Dclass, w)
-#     w -= gradient
-#     print(w)
-#     if abs(np.sum(gradient)) < 3*10e-3:
-#         break
-#
-# result = np.matmul(phi.T, w)
-#
-# prediction = []
-# for i in result:
-#     prediction.append(1) if i >= 1/2 else prediction.append(0)
-#
-# output_confusion_matrix(Dclass, prediction)
-#
-#
-# color_pre = convert_color(prediction)
-# color_gt = convert_color(Dclass)
-# # print(convert_color(prediction))
-#
-#
-# plt.subplot(131)
-# plt.title('Ground Truth')
-# plt.scatter(Dx, Dy, color=color_gt)
-#
-# plt.subplot(132)
-# plt.title('Gradient Descent')
-# plt.scatter(Dx, Dy, color=color_pre)
-# plt.show()
+    return prediction
 
 
+def newton(phi, Dclass):
+    lr = 0.005
+    nt_times = 0
+    w = build_random_matrix(3)
 
-# print(compute_gradient(phi, Dclass, w))
+    while True:
+        D = compute_D(np.matmul(w.T, phi))
+        H = np.matmul(np.matmul(phi, D), phi.T)
+        gradient = compute_gradient(phi, Dclass, w)
 
-# gradient = compute_gradient(A, A.T, Dy, w).reshape(3, 1)
-# print(w - gradient)
-# w = w - gradient
-# gradient = compute_gradient(A, A.T, Dy, w).reshape(3, 1)
-# print(w-gradient)
-# print(logistic_function(0))
+        if np.linalg.det(H) == 0:
+            if nt_times > 1000 and abs(np.sum(gradient)) < 3 * 10e-3:
+                break
+            w -= lr * gradient
+        else:
+            update = np.matmul(np.linalg.inv(H), gradient)
+            if nt_times > 1000 and abs(np.sum(update)) < 3 * 10e-3:
+                break
+            w -= lr * update
+        nt_times += 1
+
+    prediction = predict(phi, w)
+
+    print("Newton's method:")
+    print()
+    print("w:")
+    [print(i[0]) for i in w]
+    print()
+
+    output_confusion_matrix(Dclass, prediction)
+
+    return prediction
+
+
+def draw_result(gt, gd, nt):
+    color_gt = convert_color(gt)
+    color_gd = convert_color(gd)
+    color_nt = convert_color(nt)
+
+    plt.subplot(131)
+    plt.title('Ground Truth')
+    plt.scatter(Dx, Dy, color=color_gt)
+
+    plt.subplot(132)
+    plt.title('Gradient Descent')
+    plt.scatter(Dx, Dy, color=color_gd)
+    plt.show()
+
+    plt.subplot(133)
+    plt.title('Newton\'s method')
+    plt.scatter(Dx, Dy, color=color_nt)
+
+
+if __name__ == "__main__":
+    n, Dx, Dy, Dclass, phi = initial_parameter()
+    prediction_nt = newton(phi, Dclass)
+    prediction_gd = gradient_descent(phi, Dclass)
+    draw_result(Dclass, prediction_gd, prediction_nt)
