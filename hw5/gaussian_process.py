b0db2678e69eaec62de82a66b95458d1fb8e6c68
ccc870206 <ccc870206@gmail.com>
2019-12-15 14:18:42 +0800

    modify some bug and format

--- hw5/gaussian_process.py
+++ hw5/gaussian_process.py
@@ -2,28 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.optimize import minimize
-# from  scipy.stats import multivariate_normal as multi_gaussian
 
 
 def generator_gaussian(mean, var):
     return np.random.normal(mean, var**(1/2))
-
-#
-# def gaussian_distribution(mean, std, x):
-#     if std == 0:
-#         return 10e-7
-#     constant = 1/(std * pow(2*math.pi, 1/2))
-#     power = -(1/2) * pow((x-mean) / std, 2)
-#
-#     return constant * pow(math.e, power)
-
-#
-# def build_design_matrix(n, x):
-#     design_matrix = np.empty((1, n))
-#     for i in range(n):
-#         design_matrix[:, i] = np.power(x, i)
-#
-#     return design_matrix
 
 
 def diff_square(x1, x2):
@@ -45,56 +27,76 @@ def rational_quadratic_kernel(x1, x2, alpha):
 
 def func(args):
     k, y, n = args
-    # -1/2*ln|c| -1/2*y^t*c^-1*y -N/2*ln(2*pi)
-    v = lambda para:-1/2*np.log(np.linalg.det(np.power(np.power(k, 2) + 1, -para)))\
+    # 1/2*ln(c) -1/2*y^t*c^-1*y -N/2*ln(2*pi)
+    v = lambda para:-1/2*-para*np.log(np.linalg.det(np.power(k, 2) + 1)+10e-7)\
                     -1/2*np.matmul(np.matmul(y.T, np.linalg.inv(np.power(np.power(k, 2) + 1, -para))), y)\
                     -n/2*np.log(2*math.pi)
-
     return v
 
 
-def log_likelihood(k, y, n):
-    para = 1
-    c = np.power(np.power(k, 2) + 1, -para)
-    first_term = -1/2*np.log(np.linalg.det(c))
-    second_term = -1/2*np.matmul(np.matmul(y.T, np.linalg.inv(c)), y)
-    third_term = -n/2*np.log(2*math.pi)
-    # third_term = -c.shape[0]/2*math.log(2*math.pi)
-
-    return first_term+second_term+third_term
+def con():
+    cons = ({'type': 'ineq', 'fun': lambda para: para})
+    return cons
 
 
-f = open('input.data', 'r')
-data = np.array([])
-for i in f.readlines():
-    data = np.append(data, float(i.split()[0]))
-    data = np.append(data, float(i.split()[1]))
+def load_data():
+    f = open('input.data', 'r')
+    data = np.array([])
+    for i in f.readlines():
+        data = np.append(data, float(i.split()[0]))
+        data = np.append(data, float(i.split()[1]))
 
-data = data.reshape((34, 2))
-x = data[:, 0]
-y = data[:, 1]
-
-noise = generator_gaussian(0, 1/5)
-
-test = np.linspace(-60, 60, num=1000)
-args = (diff_square(x, x), y, x.shape[0])
-x0 = np.asarray((1))
-res = minimize(func(args), x0, method='SLSQP')
+    data = data.reshape((34, 2))
+    x = data[:, 0]
+    y = data[:, 1]
+    return x, y
 
 
-alpha = res.x[0]
-c = rational_quadratic_kernel(x, x, alpha)
-k_x_test = rational_quadratic_kernel(x, test, alpha)
-k_x_test_t = k_x_test.T
-k_test_test = rational_quadratic_kernel(test, test, alpha)
+def optimize_process(x, y):
+    args = (diff_square(x, x), y, x.shape[0])
+    cons = con()
+    x0 = np.asarray((10e-7))
+    res = minimize(func(args), x0, method='SLSQP', constraints=cons)
 
-mean = np.matmul(np.matmul(k_x_test_t, np.linalg.inv(c)), y)
-var = (k_test_test + noise) - np.matmul(np.matmul(k_x_test_t, np.linalg.inv(c)), k_x_test)
-var = np.diag(var)
+    return res.x[0]
 
 
-plt.scatter(data[:, 0], data[:, 1])
-plt.plot(test, mean, color="black")
-plt.fill_between(test, mean-2*var, mean+2*var, facecolor="red", alpha=0.3)
+def predict_process(x, alpha, beta_inv):
+    c = rational_quadratic_kernel(x, x, alpha)
+    k_x_test = rational_quadratic_kernel(x, test, alpha)
+    k_x_test_t = k_x_test.T
+    k_test_test = rational_quadratic_kernel(test, test, alpha)
 
-plt.show()
+    # mean = k_x_test_t * c^-1 * y
+    mean = np.matmul(np.matmul(k_x_test_t, np.linalg.inv(c)), y)
+    # var = (k_test_test + beta^-1) - k_x_test_t * c^-1 *
+    var = (k_test_test + beta_inv) - np.matmul(np.matmul(k_x_test_t, np.linalg.inv(c)), k_x_test)
+    var = np.diag(var)
+
+    return mean, var
+
+
+def gaussian_process(x, y, beta_inv):
+    alpha = optimize_process(x, y)
+    mean, var = predict_process(x, alpha, beta_inv)
+    return mean, var
+
+
+def visualize_result(x, y, mean, var):
+    plt.scatter(x, y)
+    plt.plot(test, mean, color="black")
+    plt.fill_between(test, mean - 2 * var, mean + 2 * var, facecolor="red", alpha=0.3)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    x, y = load_data()
+    beta_inv = 1/5
+    noise = generator_gaussian(0, beta_inv)
+    test = np.linspace(-60, 60, num=100)
+
+    mean, var = gaussian_process(x, y, beta_inv)
+
+    visualize_result(x, y, mean, var)
+
