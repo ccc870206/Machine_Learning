8ce9439bd51ffe8e9f79e461620b8898a3f6b49e
ccc870206 <ccc870206@gmail.com>
2019-12-25 21:02:17 +0800

    fater version

--- hw6/kernel_kmeans.py
+++ hw6/kernel_kmeans.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 from scipy.spatial.distance import pdist, squareform
 import math
 import numba as nb
@@ -27,20 +26,62 @@ def kernel(s1, s2, x1, x2, gamma_s, gamma_c):
     return math.e**total
 
 
+def matrix_kernel(s1, s2, x1, x2, gamma_s, gamma_c):
+    # s1 = np.array(s1)
+    # s2 = np.array(s2)
+    coord = -gamma_s * np.sum(np.power(s2-s1, 2), axis=1)
+    color = -gamma_c * np.sum(np.power(x2-x1, 2), axis=1)
+    total = coord+color
+    # print((math.e**total).shape)
+    return math.e**total
+
+
+def similarity_matrix(x, position, gamma_s, gamma_c):
+    # print("begin")
+    # coord = np.array([[i, j] for i in range(x.shape[0]) for j in range(x.shape[1])])
+    # print("end")
+    d_coord = -gamma_s * pdist(position, 'sqeuclidean')
+    # print(squareform(d_coord))
+    # print(coord)
+    # print(coord.shape)
+    # print(squareform(pdist(coord, 'sqeuclidean')))
+    color = x.reshape(-1, 3)
+    d_color = -gamma_c * pdist(color, 'sqeuclidean')
+    # print(squareform(d_color))
+    d_total = d_coord + d_color
+    # print(d_total)
+    # print("np exp",np.exp(d_total))
+    return squareform(np.exp(d_total))
+
+
 # @nb.jit()
 def intra_cluster_distance(x, c, size):
     n_x = c.shape[0]
     n_c = c.shape[1]
 
     total = np.zeros(n_c)
+    # for j in range(n_c):
+    #     c_list = np.where(c[:, j] == 1)[0]
+    #     c_sum = c[:, j].sum()
+    #     for i in c_list:
+    #         for k in c_list:
+    #             # print(kernel((i // size, i % size), (k // size, k % size), x[i], x[k], 0.01, 0.01))
+    #             total[j] += (kernel((i // size, i % size), (k // size, k % size), x[i], x[k], 0.01, 0.1)/c_sum/c_sum)
+    #             # print(total[j])
+    # print(total)
     for j in range(n_c):
         c_list = np.where(c[:, j] == 1)[0]
+        x_in_c = x[c_list]
+        p_in_c = points[c_list]
         c_sum = c[:, j].sum()
-        for i in c_list:
-            for k in c_list:
-                # print(kernel((i // size, i % size), (k // size, k % size), x[i], x[k], 0.01, 0.01))
-                total[j] += (kernel((i // n_x, i % n_x), (k // n_x, k % n_x), x[i], x[k], 0.01, 0.1)/c_sum/c_sum)
-                # print(total[j])
+        # print(p_in_c)
+        total[j] = (similarity_matrix(x_in_c, p_in_c, 0.01, 0.1).sum()+len(c_list))/c_sum/c_sum
+
+        # for i in c_list:
+        #     for k in c_list:
+        #         # print(kernel((i // size, i % size), (k // size, k % size), x[i], x[k], 0.01, 0.01))
+        #         total[j] += (kernel((i // size, i % size), (k // size, k % size), x[i], x[k], 0.01, 0.1)/c_sum/c_sum)
+        #         # print(total[j])
     # print(total)
     return total
 
@@ -54,19 +95,57 @@ def compute_distance(xi, si, x, c, size):
     # print(distance)
     # kernel(si, (0 // n_x, 0 % n_x), xi, x[0], 0.01, 0.01)
 
-    for j in range(n_c):
-        second_term = np.array([kernel(si, (i//size,i%size), xi, x[i], 0.01, 0.1) for i in np.where(c[:, j] == 1)[0]])
-        # print(second_term.sum())
-        second_term = -2*second_term.sum()/c[:, j].sum()
-        distance[j] += second_term
+    # for j in range(n_c):
+    #     second_term = np.array([kernel(si, (i//size,i%size), xi, x[i], 0.01, 0.1) for i in np.where(c[:, j] == 1)[0]])
+    #     # print(second_term.sum())
+    #     second_term = -2*second_term.sum()/c[:, j].sum()
+    #     distance[j] += second_term
 
     # for j in range(n_c):
     #     second_term = 0
+    #     sum_c = c[:, j].sum()
+    #
     #     for i in np.where(c[:, j] == 1)[0]:
-    #         second_term += kernel(si, (i//size,i%size), xi, x[i], 0.01, 0.01)
+    #         second_term = kernel(si, (i//size,i%size), xi, x[i], 0.01, 0.1)
     #         # print(second_term.sum())
-    #         second_term = -2*second_term/c[:, j].sum()
+    #         second_term = -2*second_term/sum_c
     #         distance[j] += second_term
+    # print("origin", distance)
+
+    for j in range(n_c):
+        second_term = 0
+        sum_c = c[:, j].sum()
+        c_list = np.where(c[:, j] == 1)[0]
+        # print("cluster", c_list.shape)
+        c_in_x = x[c_list]
+        p_in_x = points[c_list]
+
+        second_term = (matrix_kernel(np.array(si), p_in_x, xi, c_in_x, 0.01, 0.1).sum()+1)
+        # for i in np.where(c[:, j] == 1)[0]:
+        #     second_term += kernel(si, (i//size,i%size), xi, x[i], 0.001, 1)
+        #     # print(second_term.sum())
+        second_term = -2*second_term/sum_c
+        distance[j] += second_term
+    print("modify", distance)
+
+    # distance = np.ones(n_c)
+    # for j in range(n_c):
+    #     second_term = 0
+    #     sum_c = c[:, j].sum()
+    #
+    #     c_list = np.where(c[:, j] == 1)[0]
+    #     c_in_x = np.concatenate((xi.reshape(1, -1), x[c_list]))
+    #     p_in_x = np.concatenate((np.array(si).reshape(1, -1), points[c_list]))
+    #
+    #     second_term += ((similarity_matrix(c_in_x, p_in_x, 0.01, 0.1)[0]/sum_c).sum()+1/sum_c)
+    #     distance[j] += second_term
+    # print(distance)
+        # for i in np.where(c[:, j] == 1)[0]:
+        #     second_term += kernel(si, (i // size, i % size), xi, x[i], 0.01, 0.1)
+        #     # print(second_term.sum())
+        #     second_term = -2 * second_term / sum_c
+        #     distance[j] += second_term
+
 
     # for j in range(n_c):
     #     third_term = [kernel((i//n_x,i%n_x), (k//size,k%size), x[i], x[k], 0.01, 0.01) \
@@ -96,20 +175,21 @@ img*=255
 plt.show()
 
 
-img = img[:20, :20]
+# img = img[:20, :20]
 
 x = img.reshape(-1, 3)
 n_size = img.shape[0]
 points = np.array([[i, j] for i in range(n_size) for j in range(n_size)])
 
 n_x = x.shape[0]
-n_cluster = 2
+n_cluster = 4
 
 c = initial(n_x, n_cluster)
 
-for times in range(10):
+for times in range(2):
     fix_term = intra_cluster_distance(x, c, n_size)
     for i in range(n_x):
+        # i = n_x -1
         distance = compute_distance(x[i], (i//n_size, i%n_size), x, c, n_size) + fix_term
         # print(distance)
         c[i, :] = 0
@@ -129,8 +209,8 @@ for times in range(10):
         else:
             color_arr[i] = 'yellow'
 
-    plt.scatter(points[:, 0], points[:, 1], color=color_arr, alpha=0.3)
-    plt.savefig('/Users/yen/Work/ml_hw6_pic/'+'image2_'+str(times)+'.png')
+    plt.scatter(points[:, 0], points[:, 1], color=color_arr, alpha=0.3, s=5)
+    # plt.savefig('/Users/yen/Work/ml_hw6_pic/'+'image2_'+str(times)+'.png')
     plt.show()
 
 
