b4a44dfe73fe48a6aebd212575fa402585d49f60
ccc870206 <ccc870206@gmail.com>
2020-01-15 06:31:58 +0800

    hand in version

--- hw7/lda.py
+++ hw7/lda.py
@@ -25,38 +25,34 @@ def load_pkl_data(filename):
 
 def read_image(filename):
     with open(filename, 'rb') as f:
-        # magic_number = f.readline().strip().decode('utf-8')
-        # info = f.readline().strip().decode('utf-8').split(' ')
-        # width, height = f.readline().strip().decode('utf-8').split(' ')
-        # width = int(width)
-        # height = int(height)
-        # maxval = f.readline().strip().decode('utf-8')
-        #
-        # img = np.zeros((height, width))
-        # img[:, :] = [[ord(f.read(1)) for j in range(width)] for i in range(height)]
-        # # print(img.shape)
-        # # img = cv2.resize(img, (100, 100))
-        # # img/=255
+        magic_number = f.readline().strip().decode('utf-8')
+        info = f.readline().strip().decode('utf-8').split(' ')
+        width, height = f.readline().strip().decode('utf-8').split(' ')
+        width = int(width)
+        height = int(height)
+        maxval = f.readline().strip().decode('utf-8')
+
+        img = np.zeros((height, width))
+        img[:, :] = [[ord(f.read(1)) for j in range(width)] for i in range(height)]
+        # print(img.shape)
+        img = cv2.resize(img, (100, 100))
+        img/=255
         # imgplot = plt.imshow(img, cmap='gray')
         # plt.show()
 
-        img = cv2.imread(filename, 0)
         return img
 
 
 def load_data(dir, type):
     filelist = sorted(os.listdir(dir))
 
-    print(filelist)
-    # print(filelist)
     lenth = len(filelist)
     img_list = np.zeros((lenth, 100, 100))
     # print(lenth)
     for i in range(lenth):
         img = read_image(os.path.join(dir, filelist[i]))
         img_list[i, :] = img
-    # print(img_list)
-    # print(img_list.shape)
+
     save_pkl_data(img_list, type)
 
     return img_list
@@ -73,32 +69,21 @@ def load_label(dir, type):
     return label
 
 
-# def between_class():
-
-
-
 start = timeit.default_timer()
 if __name__ == '__main__':
-    # load_data(dir_test, 'test')
-
     image_train = load_pkl_data('train').astype(int)
     image_test = load_pkl_data('test')
     label_train = load_pkl_data('label_train')
 
+
     n_label = 3
-    # load_label(dir_train, 'train')
+
     image25 = image_train[:25].reshape(25, -1).T
     label25 = np.array(label_train[:25])
 
-    # total_mean = image25
-    # print(image25)
-    # print(label25)
 
-
-    # # between-class-scatter
+    # between-class scatter
     m_total = np.mean(image25, axis=1).reshape(-1,1)
-    print(m_total, m_total.shape)
-
 
     m_cluster = np.zeros((10000, n_label))
     n_c = np.zeros(n_label)
@@ -114,34 +99,23 @@ if __name__ == '__main__':
     for j in range(n_label):
         diff = m_cluster-m_total
         Sb += n_c[j] * np.matmul(diff, diff.T)
-    print(Sb, Sb.shape)
 
+
+    # within-class scatter
     Sw = np.zeros((10000,10000))
     for j in range(n_label):
         c_list = np.where(label25 == j + 1)[0]
         c_in_x = image25[:, c_list]
         diff = c_in_x - m_cluster[:, j].reshape(-1, 1)
         Sw += np.matmul(diff, diff.T)
-    print(Sw, Sw.shape)
 
     target_matrix = np.matmul(np.linalg.inv(Sw+0.1), Sb)
 
-    print(target_matrix)
-    save_pkl_data(target_matrix, 'target_m')
+
     e_val, e_vector = np.linalg.eig(target_matrix)
-    save_pkl_data(target_matrix, 'lda_eval')
-    save_pkl_data(target_matrix, 'lda_evec')
 
-
-    # # within-class-scatter
-
-
-
-
-    # print(m_cluster, m_cluster.shape)
-
-
-
+    sorted_index = np.argsort(e_val)[::-1]
+    e_vector = e_vector[:, sorted_index]
 
     stop = timeit.default_timer()
     print('Time: ', stop - start)
--- hw7/pca.py
+++ hw7/pca.py
@@ -23,22 +23,22 @@ filename = dir_train+'subject01.centerlight.pgm'
 
 def read_image(filename):
     with open(filename, 'rb') as f:
-        # magic_number = f.readline().strip().decode('utf-8')
-        # info = f.readline().strip().decode('utf-8').split(' ')
-        # width, height = f.readline().strip().decode('utf-8').split(' ')
-        # width = int(width)
-        # height = int(height)
-        # maxval = f.readline().strip().decode('utf-8')
-        #
-        # img = np.zeros((height, width))
-        # img[:, :] = [[ord(f.read(1)) for j in range(width)] for i in range(height)]
-        # # print(img.shape)
-        # # img = cv2.resize(img, (100, 100))
-        # # img/=255
+        magic_number = f.readline().strip().decode('utf-8')
+        info = f.readline().strip().decode('utf-8').split(' ')
+        width, height = f.readline().strip().decode('utf-8').split(' ')
+        width = int(width)
+        height = int(height)
+        maxval = f.readline().strip().decode('utf-8')
+
+        img = np.zeros((height, width))
+        img[:, :] = [[ord(f.read(1)) for j in range(width)] for i in range(height)]
+        # print(img.shape)
+        img = cv2.resize(img, (100, 100))
+        img/=255
         # imgplot = plt.imshow(img, cmap='gray')
         # plt.show()
 
-        img = cv2.imread(filename, 0)
+        # img = cv2.imread(filename, 0)
         return img
 
 
@@ -51,9 +51,9 @@ def load_data(dir, type):
     for i in range(lenth):
         img = read_image(os.path.join(dir, filelist[i]))
         img_list[i, :] = img
-    # print(img_list)
-    # print(img_list.shape)
+
     save_pkl_data(img_list, type)
+    return img_list
 
 
 def load_label(dir, type):
@@ -77,11 +77,6 @@ def load_pkl_data(filename):
     with open(filename+'.pkl', 'rb') as f:
         w = pkl.load(f)
     return w
-
-
-def similarity_matrix(x):
-    # d_coord = pdist(x, 'sqeuclidean')
-    return squareform(pdist(x, 'sqeuclidean'))
 
 
 def rbf_kernel(x, gamma):
@@ -133,43 +128,38 @@ def reconstruction(w, matrix):
 
 start = timeit.default_timer()
 if __name__ == '__main__':
-    # image_train = load_data(dir_train, 'train')
-    # image_test = load_data(dir_test, 'test')
+    image_train = load_data(dir_train, 'train')
+    image_test = load_data(dir_test, 'test')
+    label_train = load_label(dir_train, 'train')
+    label_test = load_label(dir_test, 'test')
 
-    # label_train = load_label(dir_train, 'train')
-    # label_test = load_label(dir_test, 'test')
-
-
-    image_train = load_pkl_data('train').astype('int')
-    image_test = load_pkl_data('test')
-
-    label_train = load_pkl_data('label_train')
-    label_test = load_pkl_data('label_test')
 
     dim = 25
 
+    # first 25 images
     image25 = image_train[:25].reshape(25, -1).T
 
     c = covariance_matrix(image25)
 
+    # compute eigenvalue, eigenvector
     e_value, e_vector = np.linalg.eigh(c)
     sorted_index = np.argsort(e_value)[::-1]
     e_vector = e_vector[:, sorted_index]
 
+    # choose principle component
     w = e_vector[:, :dim + 1]
-
+    # draw eigenface
     eigenface(w, dim)
 
-
+    # random choose 10 images
     random_index = np.random.choice(25, 10)
     image10 = image25[:, random_index]
+    # reconstruct the face
     reconstruction(w, image10)
 
-
+    # face recognition
     train = image_train.reshape(135, -1).T
     test = image_test.reshape(30, -1).T
-
-
 
     r_train = np.matmul(w.T, train)
     r_test = np.matmul(w.T, test)
@@ -184,6 +174,7 @@ if __name__ == '__main__':
         print("truth:", label_test[i])
         if label_train[idx] == label_test[i]:
             acc +=1
+    print("wrong", 30-acc, "/", 30)
     print("acc:", acc/30*100, "%")
 
 
